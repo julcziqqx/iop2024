de9fd6708b162945262e0c6b5cd9068e136192c3
julcziqqx <julia.koscielak.05@gmail.com>
2024-12-01 15:12:39 +0100

    Dodano funkcje nwd do pliku podzielnosc.cpp

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 
+int nwd(int a , int b){
+	int pom;
+	while(b != 0){
+		pom = b;
+		b = a % b;
+		a = pom;
+	}
+	return a;
+}
+
 int main(){
 	int a,b;
 	std::cin >> a >> b;
 
-	std::cout << a << " " << b << std::endl;
+	std::cout << a << " " << b << " nwd: " << nwd(a,b) << std::endl;
 
 	return 0;
 }
