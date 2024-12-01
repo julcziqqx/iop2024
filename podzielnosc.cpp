9ea97c3d78969e124390c3da4c06ab594f404e5d
julcziqqx <julia.koscielak.05@gmail.com>
2024-12-01 15:19:37 +0100

    Dodano sprawdzanie czy podane liczby sa dodatnie

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -3,7 +3,10 @@
 int main(){
 	int a,b;
 	std::cin >> a >> b;
-
+	if(a <= 0 || b <= 0) {
+		std::cout << "Liczby musza byc dodatnie" << std::endl;
+		return 1;
+	}
 	std::cout << a << " " << b << std::endl;
 
 	return 0;
